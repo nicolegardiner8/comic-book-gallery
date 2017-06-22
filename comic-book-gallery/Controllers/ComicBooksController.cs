d07b3181e8b3bc4fcca7b6ff7c6a792a7979d420
nicolegardiner8 <nicolemgardiner8@gmail.com>
2017-06-22 15:38:55 -0400

    Added comic book detail view

--- comic-book-gallery/Controllers/ComicBooksController.cs
+++ comic-book-gallery/Controllers/ComicBooksController.cs
@@ -10,12 +10,7 @@ namespace comic_book_gallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Thursday)
-            {
-                return Redirect("/");
-                return new RedirectResult("/");
-            }
-            return Content("Hello from the comic books controller page!");
+            return View();
         }
     }
 }
