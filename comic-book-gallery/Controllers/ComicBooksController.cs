70561ff71fc540b8dedbf447a9925d310ec0b36d
nicolegardiner8 <nicolemgardiner8@gmail.com>
2017-06-22 14:58:50 -0400

    Added detail action method

--- comic-book-gallery/Controllers/ComicBooksController.cs
+++ comic-book-gallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace comic_book_gallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "Hello from the comic books controller!";
+        }
     }
 }
